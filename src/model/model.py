10686e3a45a87a780eca9777503a8ec6960529c8
Qoorb <rborn3243@gmail.com>
2024-06-10 18:41:39 +0700

    update model

--- src/model/model.py
+++ src/model/model.py
@@ -1,31 +1,29 @@
-import random
-import datetime
 import os
 import os.path as op
+import datetime
+import random
 
-import PIL.ImageOps
-import numpy as np
 import PIL.Image
 import torch
 import torchvision.transforms.functional as TF
-from diffusers import (
-    AutoencoderKL,
-    EulerAncestralDiscreteScheduler,
-    StableDiffusionXLAdapterPipeline,
-    T2IAdapter,
-)
+
+from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline, AutoencoderKL
+from diffusers import DDIMScheduler, EulerAncestralDiscreteScheduler
+from diffusers.utils import load_image
+from huggingface_hub import HfApi
+from pathlib import Path
+from PIL import Image, ImageOps
+import torch
+import numpy as np
 
 
 MAX_SEED = np.iinfo(np.int32).max
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Project running on device: ", device)
 
 style_list = [
     {
         "name": "(No style)",
         "prompt": "{prompt}",
-        "negative_prompt": "",
+        "negative_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
     },
     {
         "name": "Cinematic",
@@ -78,11 +76,9 @@ styles = {k["name"]: (k["prompt"], k["negative_prompt"]) for k in style_list}
 STYLE_NAMES = list(styles.keys())
 DEFAULT_STYLE_NAME = "(No style)"
 
-
 def apply_style(style_name: str, positive: str, negative: str = "") -> tuple[str, str]:
     p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
     return p.replace("{prompt}", positive), n + negative
-
 
 def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
     if randomize_seed:
@@ -90,25 +86,24 @@ def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
     return seed
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class Model:
     def __init__(self) -> None:
-        self.model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-        self.adapter = T2IAdapter.from_pretrained(
-            "TencentARC/t2i-adapter-sketch-sdxl-1.0", torch_dtype=torch.float16, variant="fp16"
+        self.controlnet = ControlNetModel.from_pretrained(
+            "xinsir/controlnet-scribble-sdxl-1.0",
+            torch_dtype=torch.float16
         )
-        self.scheduler = EulerAncestralDiscreteScheduler.from_pretrained(
-            self.model_id, subfolder="scheduler"
-        )
-        self.pipe = StableDiffusionXLAdapterPipeline.from_pretrained(
-            self.model_id,
-            vae=AutoencoderKL.from_pretrained(
-                "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
-            ),
-            adapter=self.adapter,
-            scheduler=self.scheduler,
+
+        self.vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+
+        self.pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "sd-community/sdxl-flash",
+            controlnet=self.controlnet,
+            vae=self.vae,
             torch_dtype=torch.float16,
-            variant="fp16",
         )
+        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
         self.pipe.to(device)
 
     # def latents_to_rgb(self, latents):
@@ -140,40 +135,36 @@ class Model:
         prompt: str = '',
         negative_prompt: str = '',
         style_name: str = DEFAULT_STYLE_NAME,
-        num_steps: int = 2,
+        num_steps: int = 25,
         guidance_scale: float = 5,
-        adapter_conditioning_scale: float = 0.8,
-        adapter_conditioning_factor: float = 0.8,
-        seed: int = 0
+        controlnet_conditioning_scale: float = 1.0,
+        seed: int = 0,
     ) -> PIL.Image.Image:
-        seed = randomize_seed_fn(seed, True)
+        # seed = randomize_seed_fn(seed, True)
+        
+        image = PIL.ImageOps.invert(image.convert("RGB").resize((1024, 1024)))
 
         # logging pics before processing
         if not op.exists(f'images/{style_name}/'): os.mkdir(f'images/{style_name}')
         image.save(f'images/{style_name}/{str(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))}_before.png')
-
-        image = PIL.ImageOps.invert(image.convert("RGB").resize((1024, 1024)))
-        image = TF.to_tensor(image) > 0.5
-        image = TF.to_pil_image(image.to(torch.float32))
-
+        
         prompt, negative_prompt = apply_style(style_name, prompt, negative_prompt)
-
+    
         generator = torch.Generator(device=device).manual_seed(seed)
         
         out = self.pipe(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                image=image,
-                num_inference_steps=num_steps,
-                generator=generator,
-                guidance_scale=guidance_scale,
-                adapter_conditioning_scale=adapter_conditioning_scale,
-                adapter_conditioning_factor=adapter_conditioning_factor,
-                # callback_on_step_end=self.decode_tensors,
-                # callback_on_step_end_tensor_inputs=["latents"],
-            ).images[0]
-        
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        image=image,
+        num_inference_steps=num_steps,
+        generator=generator,
+        controlnet_conditioning_scale=controlnet_conditioning_scale,
+        guidance_scale=guidance_scale,
+        # callback_on_step_end=self.decode_tensors,
+        # callback_on_step_end_tensor_inputs=["latents"],
+        ).images[0]
+
         # logging pics after processing
         out.save(f'images/{style_name}/{str(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))}_after.png')
-        
-        return out.resize((600, 600)) # resize pics
+    
+        return out.resize((600, 600))
